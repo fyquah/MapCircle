fdcb3c8654fd325399356387418dcf09e556a2fc
fyquah95 <quah.fy95@gmail.com>
2014-06-13 11:18:28 +0800

    Gitignore test

--- app/Config/database.php
+++ app/Config/database.php
@@ -67,9 +67,9 @@ class DATABASE_CONFIG {
 	public $default = array(
 		'datasource' => 'Database/Mysql',
 		'persistent' => false,
-		'host' => '127.11.8.130',
-		'login' => 'admintWMWEge',
-		'password' => 'ELs2Cj4E8MC6',
+		'host' => 'localhost',
+		'login' => 'root',
+		'password' => '123456',
 		'database' => 'proj3',
 		'prefix' => '',
 		//'encoding' => 'utf8',
